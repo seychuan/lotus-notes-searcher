7eaec1db7fe5742c5827f7ade9ac7311dabac95b
seychuan <seychuan@gmail.com>
2018-04-16 09:55:51 +0800

    Init: First push in for lotus notes searcher project.

---
+++ src/LotusNotesReader/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace LotusNotesReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnOpenNsf = new System.Windows.Forms.Button();
+            this.txtNsfFilePath = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.cboView = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(13, 37);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbOutput.Location = new System.Drawing.Point(13, 65);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.Size = new System.Drawing.Size(919, 530);
+            this.rtbOutput.TabIndex = 1;
+            this.rtbOutput.Text = "";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(94, 39);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(178, 20);
+            this.txtSearch.TabIndex = 2;
+            // 
+            // btnOpenNsf
+            // 
+            this.btnOpenNsf.Location = new System.Drawing.Point(13, 8);
+            this.btnOpenNsf.Name = "btnOpenNsf";
+            this.btnOpenNsf.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenNsf.TabIndex = 3;
+            this.btnOpenNsf.Text = "Open Nsf";
+            this.btnOpenNsf.UseVisualStyleBackColor = true;
+            this.btnOpenNsf.Click += new System.EventHandler(this.btnOpenNsf_Click);
+            // 
+            // txtNsfFilePath
+            // 
+            this.txtNsfFilePath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNsfFilePath.Location = new System.Drawing.Point(94, 10);
+            this.txtNsfFilePath.Name = "txtNsfFilePath";
+            this.txtNsfFilePath.ReadOnly = true;
+            this.txtNsfFilePath.Size = new System.Drawing.Size(638, 20);
+            this.txtNsfFilePath.TabIndex = 4;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "NSF File|*.nsf";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // cboView
+            // 
+            this.cboView.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cboView.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboView.FormattingEnabled = true;
+            this.cboView.Location = new System.Drawing.Point(738, 10);
+            this.cboView.Name = "cboView";
+            this.cboView.Size = new System.Drawing.Size(194, 21);
+            this.cboView.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 607);
+            this.Controls.Add(this.cboView);
+            this.Controls.Add(this.txtNsfFilePath);
+            this.Controls.Add(this.btnOpenNsf);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.rtbOutput);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lotus Notes Search Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.RichTextBox rtbOutput;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnOpenNsf;
+        private System.Windows.Forms.TextBox txtNsfFilePath;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ComboBox cboView;
+    }
+}
+
---
+++ src/LotusNotesReader/Form1.cs
@@ -0,0 +1,274 @@
+﻿using Domino;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LotusNotesReader
+{
+    public partial class Form1 : Form
+    {
+        NotesSession session = new NotesSession();
+        NotesDatabase db = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            session.Initialize("");
+        }
+
+        private void ListAvailableView()
+        {
+            cboView.Items.Clear();
+            rtbOutput.Clear();
+
+            db = session.GetDatabase("", txtNsfFilePath.Text, false);
+
+            foreach (NotesView view in GetNotesView(db))
+            {
+                if (view != null)
+                {
+                    cboView.Items.Add(view.Name);
+                }
+            }
+
+            if (cboView.Items.Count > 0) cboView.SelectedIndex = 0;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (txtNsfFilePath.Text.Trim() == string.Empty)
+            {
+                MessageBox.Show("Please select a nsf file");
+                return;
+            }
+
+
+
+            btnOpenNsf.Enabled = false;
+            btnSearch.Enabled = false;
+            txtSearch.Enabled = false;
+            cboView.Enabled = false;
+
+            try
+            {
+                rtbOutput.Clear();
+
+                string searchText = txtSearch.Text.ToLower();
+
+                //NotesSession session = new NotesSession();
+                //session.Initialize("");
+                ////NotesDatabase db = session.GetDatabase("", @"D:\Personal\Tools\LotusNotesReader\LotusNotesReader\Database\log2.nsf", false);
+                //NotesDatabase db = session.GetDatabase("", txtNsfFilePath.Text, false);
+                List<string> universalIDList = new List<string>();
+
+
+                foreach (NotesView view in GetNotesView(db))
+                {
+                    if (view == null) continue;
+
+                    if (cboView.SelectedIndex >= 0)
+                    {
+                        if (view.Name != cboView.SelectedItem.ToString()) continue;
+                    }
+
+
+                    foreach (var entry in GetNotesViewEntry(view))
+                    {
+                        if (entry == null) continue;
+
+                        // Skip the entry if already processed
+                        if (universalIDList.Contains(entry.UniversalID)) continue;
+                        universalIDList.Add(entry.UniversalID);
+
+                        StringBuilder sb = new StringBuilder();
+                        StringBuilder sbEvents = new StringBuilder();
+
+                        string updatedBy = string.Empty;
+                        string startTime = string.Empty;
+                        string finishTime = string.Empty;
+                        string form = string.Empty;
+
+                        //sb.AppendLine($"UniversalID - {entry.UniversalID}");
+
+                        foreach (var item in GetNoteItem(entry.Document))
+                        {
+                            if (item == null) continue;
+
+                            //sb.AppendLine($"{item.Name}:\t{item.Text}");
+
+                            switch (item.Name)
+                            {
+                                //case "$UpdatedBy":
+                                //    updatedBy = $"{item.Name}:\t{item.Text}";
+                                //    break;
+                                //case "StartTime":
+                                //    startTime = $"{item.Name}:\t{item.Text}";
+                                //    break;
+                                //case "FinishTime":
+                                //    finishTime = $"{item.Name}:\t{item.Text}";
+                                //    break;
+                                //case "Form":
+                                //    form = $"{item.Name}:\t{item.Text}";
+                                //    break;
+                                case "EventList":
+                                    foreach(var eventStr in item.Text.Split(';'))
+                                    {
+                                        if (eventStr == string.Empty || eventStr.ToLower().IndexOf(searchText) >= 0)
+                                        {
+                                            sbEvents.AppendLine(eventStr);
+                                        }
+                                    }
+                                    break;
+                            }
+                        }
+
+                        //sb.AppendLine(updatedBy);
+                        //sb.AppendLine(startTime);
+                        //sb.AppendLine(finishTime);
+                        //sb.AppendLine(form);
+                        sb.Append(sbEvents.ToString());
+
+                        if (sb.Length > 0)
+                        {
+                            //if (searchText == string.Empty || sb.ToString().ToLower().IndexOf(searchText) >= 0)
+                            //{
+                            rtbOutput.Text += sb.ToString() + Environment.NewLine + Environment.NewLine;
+                            //}
+                        }
+
+                        Application.DoEvents();
+                    }
+                }
+
+                MessageBox.Show("Search completed.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error" + Environment.NewLine + ex.Message);
+            }
+            finally
+            {
+                btnOpenNsf.Enabled = true;
+                btnSearch.Enabled = true;
+                txtSearch.Enabled = true;
+                cboView.Enabled = true;
+            }
+
+
+
+            //NotesDocument doc = db.AllDocuments.GetFirstDocument();
+            //while(doc != null)
+            //{
+            //    Array itemArray = (System.Array)doc.Items;
+
+            //    StringBuilder sb = new StringBuilder();
+
+            //    for(int index = 0; index < itemArray.Length; index++)
+            //    {
+            //        NotesItem item = (NotesItem)itemArray.GetValue(index);
+            //        sb.AppendLine($"{item.Name} - {item.Text}");
+            //    }
+
+            //    richTextBox1.Text += sb.ToString();
+
+            //    //doc = db.AllDocuments.GetNextDocument();
+            //}
+
+
+
+
+            //List<NotesView> views = new List<NotesView>();
+
+            //foreach(var viewObject in viewsArray)
+            //{
+
+
+            //}
+
+        }
+
+        private IEnumerable<NotesDocument> GetNotesDocument(NotesDatabase db)
+        {
+            NotesDocumentCollection col = db.AllDocuments;
+
+            NotesDocument doc = col.GetFirstDocument();
+
+            while (doc != null)
+            {
+                yield return doc;
+
+                doc = col.GetNextDocument(doc);
+            }
+
+            yield return null;
+        }
+
+        private IEnumerable<NotesView> GetNotesView(NotesDatabase db)
+        {
+            var viewList = (object[])db.Views;
+
+            foreach(var view in viewList)
+            {
+                yield return (NotesView)view;
+
+                Application.DoEvents();
+            }
+
+            yield return null;
+        }
+
+        private IEnumerable<NotesViewEntry> GetNotesViewEntry(NotesView nv)
+        {
+            NotesViewEntryCollection col = nv.AllEntries;
+
+            for (int index = 1; index <= col.Count; index++)
+            {
+                NotesViewEntry entry = col.GetNthEntry(index);
+                yield return entry;
+
+                Application.DoEvents();
+            }
+
+            yield return null;
+        }
+
+        private IEnumerable<NotesItem> GetNoteItem(NotesDocument doc)
+        {
+            Array itemArray = (Array)doc.Items;
+
+            for(int index = 0; index < itemArray.Length; index++)
+            {
+                NotesItem item = (NotesItem)itemArray.GetValue(index);
+                yield return item;
+
+                Application.DoEvents();
+            }
+
+            yield return null;
+        }
+
+        private void btnOpenNsf_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (txtNsfFilePath.Text != openFileDialog1.FileName)
+                {
+                    if (txtNsfFilePath.Text == string.Empty || MessageBox.Show(this, "Reset?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
+                        txtNsfFilePath.Text = openFileDialog1.FileName;
+
+                        ListAvailableView();
+                    }
+                }
+            }
+        }
+    }
+}
